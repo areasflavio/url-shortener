729162b8048ce447277d69adf45d6dfa9c328a74
Flávio Arêas <vflavio9@gmail.com>
2022-01-28 17:04:07 -0300

    refactor: clean main App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-import { Link, Lock } from 'iconoir-react';
-
 import { Header } from './components/Header';
-import { Input } from './components/Input';
+import { InputsArea } from './components/InputsArea';
+import { LatestURLsList } from './components/LatestURLsList';
 
 import './styles/global.scss';
 import styles from './App.module.scss';
 
 function App() {
-  function handleShorten(shortenURL: string) {
-    console.log({ shortenURL });
-  }
-
-  function handleRecover(originURL: string) {
-    console.log({ originURL });
-  }
-
   return (
     <>
       <Header />
 
       <main className={styles.main_bg}>
         <div className={styles.main}>
-          <section className={styles.inputs_area} aria-label="Inputs area">
-            <Input
-              buttonIcon={<Link />}
-              buttonText="shorten"
-              placeholder="Write or paste the origin URL to be shorten"
-              onButtonPress={handleShorten}
-            />
+          <InputsArea />
 
-            <span>OR</span>
-
-            <Input
-              buttonIcon={<Lock />}
-              buttonText="recover"
-              placeholder="Write or paste the hash to recover the origin URL"
-              onButtonPress={handleRecover}
-            />
-          </section>
+          <LatestURLsList />
         </div>
       </main>
     </>
