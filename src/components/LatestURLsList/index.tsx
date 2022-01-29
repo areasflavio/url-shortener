f34c80453e5287f140959726fa4c50fc1248e3f3
Flávio Arêas <vflavio9@gmail.com>
2022-01-29 14:46:51 -0300

    feat: display LatestURLs from api

--- src/components/LatestURLsList/index.tsx
+++ src/components/LatestURLsList/index.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
 import { Copy } from 'iconoir-react';
+import ClipboardJS from 'clipboard';
+
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
+interface ULR {
+  _id: string;
+  hash: string;
+  originURL: string;
+  shortURL: string;
+}
+
+new ClipboardJS('.copy');
+
 const LatestURLsList: React.FC = () => {
+  const [urls, setUrls] = useState<ULR[]>([]);
+
+  useEffect(() => {
+    async function getLatestURLs() {
+      const response = await api.get('/latestURLs');
+      setUrls(response.data);
+    }
+
+    getLatestURLs();
+  }, []);
+
   return (
     <section className={styles.latest_urls} aria-label="Latest URLs">
       <h2>Latest URLs</h2>
@@ -10,51 +34,22 @@ const LatestURLsList: React.FC = () => {
       <div>
         <span>Origin URL</span>
         <ul>
-          <li>
-            <a>
-              https://github.com/areasflavio/areasflavio/areasflavio/areasflavio/areasflavio
-            </a>
-          </li>
-          <li>
-            <a>
-              https://github.com/areasflavio/areasflavio/areasflavio/areasflavio
-            </a>
-          </li>
-          <li>
-            <a>https://github.com/areasflavio</a>
-          </li>
-          <li>
-            <a>
-              https://github.com/areasflavio/areasflavio/areasflavio/areasflavio
-            </a>
-          </li>
-          <li>
-            <a>https://github.com/areasflavio</a>
-          </li>
+          {urls.map((url) => (
+            <li key={url._id}>
+              <p>{url.originURL}</p>
+            </li>
+          ))}
         </ul>
 
         <span>Short URL</span>
         <ul>
-          <li>
-            <a>https://urlshorten.com/anF1MoC8H</a>
-            <Copy />
-          </li>
-          <li>
-            <a>https://urlshorten.com/anF1MoC8H</a>
-            <Copy />
-          </li>
-          <li>
-            <a>https://urlshorten.com/anF1MoC8H</a>
-            <Copy />
-          </li>
-          <li>
-            <a>https://urlshorten.com/anF1MoC8H</a>
-            <Copy />
-          </li>
-          <li>
-            <a>https://urlshorten.com/anF1MoC8H</a>
-            <Copy />
-          </li>
+          {urls.map((url) => (
+            <li key={url._id}>
+              <p>{`http://urlshorten.com/${url.hash}`}</p>
+
+              <Copy className="copy" data-clipboard-text={url.shortURL} />
+            </li>
+          ))}
         </ul>
       </div>
     </section>
